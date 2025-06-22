5b645e15d2641a862c838f41b54ec67e279dac46
Maycon-jeferson <jefersonmaycon3@gmail.com>
2025-06-22 17:29:36 -0300

    adicionado mecanica de recoi;, e aceleracao gradual

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,31 +1,40 @@
 extends CharacterBody2D
 
-@export var speed: float = 200.0
-@export var jump_force: float = 400.0
-@export var gravity: float = 900.0
-@export var max_fall_speed: float = 1000.0
+@export var acceleration: float = 200.0
+@export var max_speed_x: float = 500.0
 
 @onready var weapon: Node2D = $Weapon
 
+var max_fall_speed: float = 1000.0
+var gravity: float = 900.0
+var jump_force: float = 350.0
+
 func _physics_process(delta: float) -> void:
+	move(delta)
+	print(velocity)
+
+func move(delta: float):
 	var direction = Vector2.ZERO
 
-	# Movimento lateral
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
 	if Input.is_action_pressed("ui_left"):
 		direction.x -= 1
 
-	# Gravidade
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	else:
-		if Input.is_action_just_pressed("ui_accept"):
-			velocity.y = -jump_force
+	if is_on_floor() and Input.is_action_just_pressed("ui_accept"):
+		velocity.y = -jump_force
 
 	if velocity.y > max_fall_speed:
 		velocity.y = max_fall_speed
 
-	# Aplica movimento
-	velocity.x = direction.x * speed
+	# MOVIMENTO COM ACELERAÇÃO
+	var target_velocity_x = direction.x * max_speed_x
+	velocity.x = move_toward(velocity.x, target_velocity_x, acceleration * delta)
+
 	move_and_slide()
+# Método para aplicar recuo vindo da arma
+func apply_recoil(force: Vector2) -> void:
+	velocity += force
--- weapon.gd
+++ weapon.gd
@@ -4,24 +4,22 @@ const bullet_scene = preload("res://src/behavior/bullet.tscn")
 @onready var muzzle = $Muzzle
 
 @export var shoot_cooldown: float = 0.3
+@export var recoil_force: float = 100.0
+@export var player: CharacterBody2D
 
 var shoot_timer: float = 0.0
 
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
 	rotate_weapon()
+
+	shoot_timer = max(shoot_timer - delta, 0.0)
 	bullet_fire()
-	shoot_timer -= delta
-	
-	
 
 func rotate_weapon():
 	rotation_degrees = wrap(rotation_degrees, 0, 360)
-	if rotation_degrees > 90 and rotation_degrees < 270:
-		scale.y = -1
-	else:
-		scale.y = 1
-		
+	scale.y = -1 if rotation_degrees > 90 and rotation_degrees < 270 else 1
+
 func bullet_fire():
 	if Input.is_action_pressed("shoot") and shoot_timer <= 0.0:
 		var bullet_instance = bullet_scene.instantiate()
@@ -29,3 +27,8 @@ func bullet_fire():
 		bullet_instance.global_position = muzzle.global_position
 		bullet_instance.rotation = rotation
 		shoot_timer = shoot_cooldown
+
+		# Aplica recuo no player
+		if player and player.has_method("apply_recoil"):
+			var recoil_direction = -transform.x.normalized()
+			player.apply_recoil(recoil_direction * recoil_force)
