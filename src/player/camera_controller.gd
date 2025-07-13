49a61bf55f089af27d7cddf509dc04b063a7135c
Maycon-jeferson <jefersonmaycon3@gmail.com>
2025-07-12 21:27:15 -0300

    sistema de zoom pelo scroll funcional

--- src/player/camera_controller.gd
+++ src/player/camera_controller.gd
@@ -1,11 +1,19 @@
 extends Camera2D
 
-var zoom_min = Vector2(.1, .1)
-var zoom_max = Vector2(2, 2)
-var zoom_speed = Vector2 (.2, .2)
-var deslizant_zoom = zoom
+var zoom_min = Vector2(.5, .5)
+var zoom_max = Vector2(3, 3)
+var zoom_speed = Vector2(.2, .2)
+var deslizant_zoom = Vector2(1, 1)  # Inicializa com zoom padrão
 
-func _process(delta: float) -> void:
+func _ready() -> void:
+	# Garante que o zoom inicial seja válido
+	zoom = Vector2(1, 1)
+
+func _process(_delta: float) -> void:
+	# Garante que o deslizant_zoom nunca seja zero ou muito pequeno
+	deslizant_zoom.x = max(deslizant_zoom.x, 0.01)
+	deslizant_zoom.y = max(deslizant_zoom.y, 0.01)
+	
 	zoom = lerp(zoom, deslizant_zoom, .2)
 
 func _input(event: InputEvent) -> void:
@@ -13,10 +21,9 @@ func _input(event: InputEvent) -> void:
 		if event.is_action_pressed("zoon_in"):
 			if deslizant_zoom > zoom_min:
 				deslizant_zoom -= zoom_speed
-			print("in")
+
 			
 		if event.is_action_pressed("zoon_out"):
 			if deslizant_zoom < zoom_max:
 				deslizant_zoom += zoom_speed
-			print("out")
 			
