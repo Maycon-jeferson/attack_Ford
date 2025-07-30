1e7a4629ef67e0ac27da2f76acaf04d35d418994
Maycon-jeferson <jefersonmaycon3@gmail.com>
2025-07-30 08:28:04 -0300

    button red comfigurado

--- src/button_red.gd
+++ src/button_red.gd
@@ -1,18 +1,18 @@
 extends Area2D
 
-var switch: bool = false
+var switch_botton_red: bool = false
 
 @onready var animate = $AnimatedSprite2D
 
 func switch_button():
-	switch = !switch
+	switch_botton_red = !switch_botton_red
 	sprinte_change()
 
 func sprinte_change():
-	if switch == false:
+	if switch_botton_red == false:
 		print("button_off")
 		animate.play("button_red")
 	
-	if switch == true:
+	if switch_botton_red == true:
 		print("button_on")
 		animate.play("button_green")
